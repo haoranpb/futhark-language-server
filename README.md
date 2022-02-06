93d978fef9e43ba9c7360c7444d349d2b071851c
haoranpb <haoranpb@outlook.com>
2022-02-06 16:18:57 +0100

    compile on hover

---
+++ README.md
@@ -0,0 +1,7 @@
+# Futhark Language Server
+
+WIP
+
+## Quick links
+
+[Language.LSP.Server doc](https://www.stackage.org/haddock/nightly-2022-02-05/lsp-1.4.0.0/Language-LSP-Server.html)
--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,57 @@
 
 module Main where
 
+import Control.Lens ((^.))
 import Control.Monad.IO.Class
 import qualified Data.Text as T
+import Futhark.Compiler (readProgramOrDie)
+import Futhark.Util.Loc
+import Language.Futhark.Query
+import Language.Futhark.Syntax (locStr, pretty)
 import Language.LSP.Server
 import Language.LSP.Types
+import Language.LSP.Types.Lens (HasUri (uri))
 import System.Log.Logger
+
+debug :: String -> LspT () IO ()
+debug msg = liftIO $ debugM "futhark" msg
 
 handlers :: Handlers (LspM ())
 handlers =
   mconcat
     [ notificationHandler SInitialized $ \_not -> do
-        sendNotification SWindowShowMessage (ShowMessageParams MtInfo "Futhark language extension initialized"),
+        liftIO $ debugM "futhark" "Initialized",
       requestHandler STextDocumentHover $ \req responder -> do
         liftIO $ debugM "futhark" "Got hover request"
-        let RequestMessage _ _ _ (HoverParams _doc pos _workDone) = req
+        let RequestMessage _ _ _ (HoverParams doc pos _workDone) = req
             Position _l _c' = pos
             rsp = Hover ms (Just range)
             ms = HoverContents $ markedUpContent "futhark-language-server" "Hello world"
             range = Range pos pos
+        let filePath = uriToFilePath $ doc ^. uri
+        debug $ show filePath
+        case filePath of
+          Just path -> do
+            debug $ show path
+            (_, imports, _) <- readProgramOrDie path
+            case atPos imports $ Pos path 1 30 0 of
+              Nothing -> debug "No information available"
+              Just (AtName qn def loc) -> do
+                debug $ "Name: " ++ show qn
+                debug $ "Position: " ++ locStr (srclocOf loc)
+                case def of
+                  Nothing -> return ()
+                  Just (BoundTerm t defloc) -> do
+                    debug $ "Type: " ++ pretty t
+                    debug $ "Definition: " ++ locStr (srclocOf defloc)
+                  Just (BoundType defloc) ->
+                    debug $ "Definition: " ++ locStr (srclocOf defloc)
+                  Just (BoundModule defloc) ->
+                    debug $ "Definition: " ++ locStr (srclocOf defloc)
+                  Just (BoundModuleType defloc) ->
+                    debug $ "Definition: " ++ locStr (srclocOf defloc)
+          Nothing -> do
+            debug "No path"
         responder (Right $ Just rsp)
     ]
 
@@ -30,7 +63,11 @@ main = do
     ServerDefinition
       { onConfigurationChange = const $ const $ Right (),
         defaultConfig = (),
-        doInitialize = \env _req -> pure $ Right env,
+        doInitialize = \env _req -> do
+          liftIO $ debugM "futhark" "Initializing"
+          -- ? is it the right pos to first compile the file
+          -- ? where to store the result
+          pure $ Right env,
         staticHandlers = handlers,
         interpretHandler = \env -> Iso (runLspT env) liftIO,
         options = defaultOptions
